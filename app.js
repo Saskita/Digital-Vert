d86924940e62743746720af0236f57f0f9182f44
Saskita <77811300+Saskita@users.noreply.github.com>
2021-02-05 14:04:11 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+    // var scroll = $(window).scrollTop();
+
+// var section = document.getElementById('section')
+// s2.addEventlistener('scroll',function(event)}
+//     console.log('scroll')
+
+//     if (scroll >= 1000) {
+//         $(".s2").addClass(".green");
+//     } else {
+//         $(".s2").removeClass(".green");
+//     }
